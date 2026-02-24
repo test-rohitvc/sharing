08d3db5ca41dc445c5cb5de841c6ef0a45311880
test-rohitvc <thehackerfast12@gmail.com>
2026-02-24 16:23:13 +0530

    Update file_encryption.py

--- file_encryption.py
+++ file_encryption.py
@@ -1,3 +1,6 @@
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 import anyio
 from fastapi.responses import StreamingResponse
 
